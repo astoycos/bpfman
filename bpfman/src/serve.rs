cd91573d814a3a64c5fa7ba111020a8bf916dc68
Andrew Stoycos <astoycos@redhat.com>
2023-12-13 12:30:55 -0500

    bpfman: use a global lazy_static var the db
    Convert to using a global lazy_static var for the root db handle. This
    will allow multiple differnet parts of the codebase to use it without
    a ton of structural changes.
    
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- bpfman/src/serve.rs
+++ bpfman/src/serve.rs
@@ -4,12 +4,9 @@
 use std::{fs::remove_file, path::Path};
 
 use bpfman_api::{
-    config::Config,
-    util::directories::{RTPATH_BPFMAN_SOCKET, STDIR_DB},
-    v1::bpfman_server::BpfmanServer,
+    config::Config, util::directories::RTPATH_BPFMAN_SOCKET, v1::bpfman_server::BpfmanServer,
 };
 use log::{debug, info};
-use sled::Config as DbConfig;
 use tokio::{
     join,
     net::UnixListener,
@@ -28,6 +25,7 @@ use crate::{
     static_program::get_static_programs,
     storage::StorageManager,
     utils::{set_file_permissions, SOCK_MODE},
+    ROOT_DB,
 };
 
 pub async fn serve(
@@ -51,17 +49,12 @@ pub async fn serve(
     let allow_unsigned = config.signing.as_ref().map_or(true, |s| s.allow_unsigned);
     let (itx, irx) = mpsc::channel(32);
 
-    let database = DbConfig::default()
-        .path(STDIR_DB)
-        .open()
-        .expect("Unable to open database");
-
-    let mut image_manager = ImageManager::new(database.clone(), allow_unsigned, irx).await?;
+    let mut image_manager = ImageManager::new(ROOT_DB.clone(), allow_unsigned, irx).await?;
     let image_manager_handle = tokio::spawn(async move {
         image_manager.run().await;
     });
 
-    let mut bpf_manager = BpfManager::new(config.clone(), rx, itx, database);
+    let mut bpf_manager = BpfManager::new(config.clone(), rx, itx);
     bpf_manager.rebuild_state().await?;
 
     let static_programs = get_static_programs(static_program_path).await?;
