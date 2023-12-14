c05df70c21c35f91b2aadab6877a30cb72dbf8ce
Andrew Stoycos <astoycos@redhat.com>
2023-12-14 12:40:04 -0500

    bpfman: re-write the database layer
    This commit is huge and it re-writes the memory layout for all Program
    related information. More specifically it removed the KernelInfo struct
    and collapses all information for bpfman programs AND other programs
    into a single Program enum. This program Enum is essentially a wrapper
    for a sled database tree. Each program will get it's own sled database tree
    and anytime a getter/setter is called on a Program it will interact with
    the database.
    
    On load we have to use a random number generator to generate a unique id
    for the program tree prior to load.  Once the program is successfully
    loaded we can then swap the root index of the database tree to the
    correct id.  From then on it's always safe to assume the tree name will
    correlate to a given program id.
    
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- bpfman/src/main.rs
+++ bpfman/src/main.rs
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of bpfman
-
-use clap::Parser;
-use sled::{Config, Db};
-use lazy_static::lazy_static;
+#[cfg(not(test))]
 use bpfman_api::util::directories::STDIR_DB;
+use clap::Parser;
+use lazy_static::lazy_static;
+use sled::{Config, Db};
 
 mod bpf;
 mod cli;
@@ -21,12 +21,20 @@ mod utils;
 
 const BPFMAN_ENV_LOG_LEVEL: &str = "RUST_LOG";
 
+#[cfg(not(test))]
 lazy_static! {
-    pub static ref ROOT_DB: Db  = 
-        Config::default()
+    pub static ref ROOT_DB: Db = Config::default()
         .path(STDIR_DB)
         .open()
         .expect("Unable to open root database");
+}
+
+#[cfg(test)]
+lazy_static! {
+    pub static ref ROOT_DB: Db = Config::default()
+        .temporary(true)
+        .open()
+        .expect("Unable to open temporary root database");
 }
 
 fn main() -> anyhow::Result<()> {
